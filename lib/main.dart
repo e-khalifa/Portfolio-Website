64a285226c50374dd32cf89b8bbd9ede3995bc8c
Esraa Khalifa <esraakhalifa208@yahoo.com>
2024-08-18 23:38:34 +0300

    Update ScreenUtil

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -18,20 +20,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
         designSize: const Size(510, 750),
-        minTextAdapt: true,
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'Esraa Khalifa - Portfolio',
-          theme: ThemeData(
-            fontFamily: "PlusJakartaSans",
-            primaryColor: ColorUtility.main,
-            appBarTheme: const AppBarTheme(
-                backgroundColor: ColorUtility.background,
-                foregroundColor: ColorUtility.main),
-            colorScheme: ColorScheme.fromSwatch(
-                backgroundColor: ColorUtility.background),
-          ),
-          home: const IntroPage(),
-        ));
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Esraa Khalifa - Portfolio',
+            theme: ThemeData(
+              fontFamily: "PlusJakartaSans",
+              primaryColor: ColorUtility.main,
+              appBarTheme: const AppBarTheme(
+                  backgroundColor: ColorUtility.background,
+                  foregroundColor: ColorUtility.main),
+              colorScheme: ColorScheme.fromSwatch(
+                  backgroundColor: ColorUtility.background),
+            ),
+            home: const IntroPage(),
+          );
+        });
   }
+}
+
+class AppScrollBehaviour extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
 }
